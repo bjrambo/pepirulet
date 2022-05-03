ad49e4586f6a52ca52ed6d0ef587f1a0fbd9e138
johnny kang <qw5414@naver.com>
2022-05-03 10:39:38 +0900

    리팩토링

--- pepirulet/Form1.cs
+++ pepirulet/Form1.cs
@@ -14,6 +14,7 @@ namespace pepiAttendance
             InitializeComponent();
         }
 
+        // 버튼 클릭할때 async 을 사용하지만 void 를 쓸수박에 없어서 Void를 사용함.
         private void button1_Click(object sender, EventArgs e)
         {
             string userId = UserId.Text;
@@ -26,74 +27,88 @@ namespace pepiAttendance
 
             try
             {
-                Driver = ChromeSetting.BrowserInit();
-                if (Driver == null)
+                using (Driver = BrowserInit())
                 {
-                    return;
-                }
-                Driver.Navigate().GoToUrl("https://www.naver.com/");
-                Driver.FindElement(By.Id("query")).SendKeys("");
-                Driver.FindElement(By.Id("query")).Click();
-                Driver.FindElement(By.Id("query")).Clear();
-                Driver.FindElement(By.Id("query")).SendKeys(OpenQA.Selenium.Keys.Backspace);
-                Driver.FindElement(By.Id("query")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Backspace);
-                Driver.FindElement(By.Id("query")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Delete);
-                char[] keyword = SEARCH_NAME.ToCharArray();
-                for (int x = 0; x < keyword.Length; x++)
-                {
-                    Driver.FindElement(By.Id("query")).SendKeys(keyword[x].ToString());
-                    Thread.Sleep(100);
-                }
-                Driver.FindElement(By.Id("query")).SendKeys(OpenQA.Selenium.Keys.Enter);
-                ChromeUtils.DelayRandom(3, 5);
+                    if (Driver == null)
+                    {
+                        return;
+                    }
+                    Driver.Navigate().GoToUrl("https://www.naver.com/");
+                    Driver.FindElement(By.Id("query")).SendKeys("");
+                    Driver.FindElement(By.Id("query")).Click();
+                    Driver.FindElement(By.Id("query")).Clear();
+                    Driver.FindElement(By.Id("query")).SendKeys(OpenQA.Selenium.Keys.Backspace);
+                    Driver.FindElement(By.Id("query")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Backspace);
+                    Driver.FindElement(By.Id("query")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Delete);
+                    char[] keyword = SEARCH_NAME.ToCharArray();
+                    for (int x = 0; x < keyword.Length; x++)
+                    {
+                        Driver.FindElement(By.Id("query")).SendKeys(keyword[x].ToString());
+                        Thread.Sleep(100);
+                    }
+                    Driver.FindElement(By.Id("query")).SendKeys(OpenQA.Selenium.Keys.Enter);
+                    ChromeUtils.DelayRandom(3, 5);
 
-                var element = Driver.FindElements(By.ClassName("info_group"))[4].FindElement(By.TagName("dd")).FindElements(By.TagName("a"));
+                    var element = Driver.FindElements(By.ClassName("info_group"))[4].FindElement(By.TagName("dd")).FindElements(By.TagName("a"));
 
-                element[1].Click();
-                Driver.SwitchTo().Window(Driver.WindowHandles.Last());
-                Driver.FindElement(By.ClassName("xlgames-login")).Click();
-                ChromeUtils.DelayRandom(1, 2);
-                Driver.FindElement(By.Name("j_username")).SendKeys("");
-                Driver.FindElement(By.Name("j_username")).Click();
-                Driver.FindElement(By.Name("j_username")).Clear();
-                Driver.FindElement(By.Name("j_username")).SendKeys(OpenQA.Selenium.Keys.Backspace);
-                Driver.FindElement(By.Name("j_username")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Backspace);
-                Driver.FindElement(By.Name("j_username")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Delete);
-                char[] charUserId = userId.ToCharArray();
-                for (int x = 0; x < charUserId.Length; x++)
-                {
-                    Driver.FindElement(By.Name("j_username")).SendKeys(charUserId[x].ToString());
-                    Thread.Sleep(100);
+                    element[1].Click();
+                    Driver.SwitchTo().Window(Driver.WindowHandles.Last());
+                    Driver.FindElement(By.ClassName("xlgames-login")).Click();
+                    ChromeUtils.DelayRandom(1, 2);
+                    Driver.FindElement(By.Name("j_username")).SendKeys("");
+                    Driver.FindElement(By.Name("j_username")).Click();
+                    Driver.FindElement(By.Name("j_username")).Clear();
+                    Driver.FindElement(By.Name("j_username")).SendKeys(OpenQA.Selenium.Keys.Backspace);
+                    Driver.FindElement(By.Name("j_username")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Backspace);
+                    Driver.FindElement(By.Name("j_username")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Delete);
+                    char[] charUserId = userId.ToCharArray();
+                    for (int x = 0; x < charUserId.Length; x++)
+                    {
+                        Driver.FindElement(By.Name("j_username")).SendKeys(charUserId[x].ToString());
+                        Thread.Sleep(100);
+                    }
+
+                    Driver.FindElement(By.Name("j_password")).SendKeys("");
+                    Driver.FindElement(By.Name("j_password")).Click();
+                    Driver.FindElement(By.Name("j_password")).Clear();
+                    Driver.FindElement(By.Name("j_password")).SendKeys(OpenQA.Selenium.Keys.Backspace);
+                    Driver.FindElement(By.Name("j_password")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Backspace);
+                    Driver.FindElement(By.Name("j_password")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Delete);
+                    char[] charPassword = password.ToCharArray();
+                    for (int x = 0; x < charPassword.Length; x++)
+                    {
+                        Driver.FindElement(By.Name("j_password")).SendKeys(charPassword[x].ToString());
+                        Thread.Sleep(100);
+                    }
+                    Driver.FindElement(By.Name("j_password")).SendKeys(OpenQA.Selenium.Keys.Enter);
+                    ChromeUtils.DelayRandom(3, 5);
+
+                    Driver.FindElement(By.ClassName("wing-banner")).Click();
+                    ChromeUtils.DelayRandom(1, 2);
+                    Driver.SwitchTo().Frame("eventFrame");
+                    Driver.FindElement(By.ClassName("link-gift")).Click();
+                    Driver.Quit();
+                    MessageBox.Show("출책을 완료하였습니다");
                 }
-
-                Driver.FindElement(By.Name("j_password")).SendKeys("");
-                Driver.FindElement(By.Name("j_password")).Click();
-                Driver.FindElement(By.Name("j_password")).Clear();
-                Driver.FindElement(By.Name("j_password")).SendKeys(OpenQA.Selenium.Keys.Backspace);
-                Driver.FindElement(By.Name("j_password")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Backspace);
-                Driver.FindElement(By.Name("j_password")).SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Delete);
-                char[] charPassword = password.ToCharArray();
-                for (int x = 0; x < charPassword.Length; x++)
-                {
-                    Driver.FindElement(By.Name("j_password")).SendKeys(charPassword[x].ToString());
-                    Thread.Sleep(100);
-                }
-                Driver.FindElement(By.Name("j_password")).SendKeys(OpenQA.Selenium.Keys.Enter);
-                ChromeUtils.DelayRandom(3, 5);
-
-                Driver.FindElement(By.ClassName("wing-banner")).Click();
-                ChromeUtils.DelayRandom(1, 2);
-                Driver.SwitchTo().Frame("eventFrame");
-                Driver.FindElement(By.ClassName("link-gift")).Click();
-                Driver.Quit();
-                Driver.Dispose();
-                MessageBox.Show("출책을 완료하였습니다");
             }
             catch (Exception ex)
             {
-                MessageBox.Show("출석하는데 문제가 발생되었습니다!");
+                MessageBox.Show($"출석하는데 문제가 발생되었습니다! 에러 메세지 {ex.Message.ToString()}");
             }
+        }
 
+        public static ChromeDriver BrowserInit()
+        {
+            string useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36";
+            ChromeOptions option = new ChromeOptions();
+            option.AddExcludedArgument("enable-automation");
+            option.AddArgument("disable-infobars");
+            option.AddArgument("--window-size=1600,1000");
+            option.AddArgument("--user-agent=" + useragent);
+
+            ChromeDriver chromeDriver = new ChromeDriver(option);
+            chromeDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+            return chromeDriver;
         }
     }
 }
